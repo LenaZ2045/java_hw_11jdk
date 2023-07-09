a254b0a46da893789d9af3c9c189607b02141f02
Lena Zimin <Bogypesn@gmail.com>
2023-07-08 22:44:25 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String...args) {
+        int number1 = 124;
+        int number2 = 24;
+        int sum = number1 + number2;
+        int subtract = number1 - number2;
+        int multiply = number1 * number2;
+        int divide = number1 / number2;
+
+        System.out.println(sum);
+        System.out.println(subtract);
+        System.out.println(multiply);
+        System.out.println(divide);
+
+    }
+}
